d9cf87ed9350ee41fcbe3177872345513e8010aa
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2023-09-20 13:30:51 +0300

    Update the animals table by setting the species column to digimon and pokemon

--- queries.sql
+++ queries.sql
@@ -30,3 +30,17 @@ BEGIN;
 UPDATE animals
 SET species = 'unspecified';
 ROLLBACK;
+
+-- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+-- Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+BEGIN;
+
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+
+COMMIT;
