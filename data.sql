d7aa76d20dc802ccb230d6863fe3de6a06c6deea
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2023-09-20 13:15:49 +0300

    update the animals table by setting the species column to unspecified and fix weight_kg for Plantmon and Boarmon

--- data.sql
+++ data.sql
@@ -10,9 +10,9 @@ VALUES
 -- Insert the new data
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Charmander', '2020-02-08', 0, false, -11),
-('Plantmon', '2021-11-15', 2, true, -5.07),
+('Plantmon', '2021-11-15', 2, true, -5.7),
 ('Squirtle', '1993-4-2', 3, false, -12.13),
 ('Angemon', '2005-06-12', 1, true, -45),
-('Boarmon', '2005-06-07', 7, true, 20.04),
+('Boarmon', '2005-06-07', 7, true, 20.4),
 ('Blossom', '1998-10-13', 3, true, 17),
 ('Ditto', '2022-05-14', 4, true, 22);
